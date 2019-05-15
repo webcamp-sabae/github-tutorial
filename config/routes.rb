c4ad20154599ae08b49ccea19547e8b89b628503
su-san1129 <deviationvalue1129@icloud.com>
2019-05-15 06:06:38 +0000

    add text routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :cds, only: [:index, :show, :create]
+resources :users, only: [:show, :edit, :update, :destroy]
+resources :carts, only: [:index, :create, :update, :destroy]
+resources :recipts, only: [:new, :create, :update, :destory]
+resources :othersadress, only: [:new, :create]
+resources :thanks, only: [:thanks]
+resources :purchases, only: [:index]
+
+
 end
