76d6a4ffd5ad25f43ffafbbd3aaeca1a340fa0fb
osuke-noguchi <noguchi.nodai@gmail.com>
2019-05-15 06:25:21 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'cds#index'
+resources :users, [:show, :edit, :update, :destroy],
+          :cds, only[:index, :show,],
+          :carts, only[:index, :create, :update, :destroy],
+          :purchases, only[:index],
+          :receipus, only[:new, :create],
+          :othesaddress, only[:new, :update],
+          :thanks, only[:thanks]
 end
